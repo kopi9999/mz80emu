be662ac84aa7b92c5e685c7813ba412ce62bf1b9
kopi999 <przem.gda@gmail.com>
2025-03-25 19:37:20 +0100

    added unit tests to testDll.

--- src/modules/test/unitTests/main.c
+++ src/modules/test/unitTests/main.c
@@ -1,25 +1,65 @@
 #include "../testdll.h"
-#include <assert.h>
+#include <stdbool.h>
 
-void testAssertionFail(){
-    assert(("testAssertionFail", 0));
+#define LENGTH 3
+
+bool testIfInstanceIsCreated(){
+    void* instance = NULL;
+    enum Error error;
+
+    error = create(&instance, NULL);
+
+    if (error) {return false;}
+    if (!instance) {return false;}
+    return true;
 }
 
-void testIfInstanceIsCreated(){
-    void* instance = NULL;
-    uint8_t* parameters;
-    *parameters = 0;
+bool testIfCreatedInstanceIsValid(){
+    struct Instance* instance;
+    enum Error error;
 
-    create(&instance, parameters);
+    error = create((void*) &instance, NULL);
 
-    assert(("testIfInstanceIsCreated", instance));
+    if (error) {return false;}
+    if (instance->value){return false;}
+    if (!instance->character){return false;}
+    if (*instance->character){return false;}
+    return true;
+}
+
+bool testInstanceCreationPararameters(){
+    struct Instance* instance;
+    uint8_t parameters[2] = {5, 10};
+    enum Error error;
+
+    error = create((void*) &instance, parameters);
+
+    if (error) {return false;}
+    if (instance->value != 5){return false;}
+    if (!instance->character){return false;}
+    if (*instance->character != 10){return false;}
+    return true;
+}
+
+void iterateTests(char* names[], bool (*funcs[])(), uint16_t length)
+{
+    uint16_t fails = 0;
+    for (size_t i = 0; i < length; ++i){
+        printf("%s: ", names[i]);
+        if (funcs[i]()){
+            printf("PASS\n");
+            continue;
+        }
+        ++fails;
+        printf("FAIL\n");
+    }
+    printf("\n%u tests ran, %u failed", length, fails);
 }
 
 int main()
 {
-    //testAssertionFail();
-    printf("testIfInstanceIsCreated: ");
-    testIfInstanceIsCreated();
-    printf("OK");
+    bool (*funcs[LENGTH])() = {testIfInstanceIsCreated, testIfCreatedInstanceIsValid, testInstanceCreationPararameters};
+    char* names[LENGTH] = {"testIfInstanceIsCreated", "testIfCreatedInstanceIsValid", "testInstanceCreationPararameters"};
+    iterateTests(names, funcs, LENGTH);
     return 0;
 }
