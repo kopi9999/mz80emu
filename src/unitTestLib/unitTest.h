ee2259e0e876559f909e4cffbcfc445a271f6561
unknown <Adam Mickiewicz>
2025-03-26 09:46:12 +0100

    added global unitTest library

---
+++ src/unitTestLib/unitTest.h
@@ -0,0 +1,10 @@
+#ifndef UNIT_TEST_LIB
+#define UNIT_TEST_LIB
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+extern void iterateTests(char* [], bool (*[])(), uint16_t);
+
+#endif
