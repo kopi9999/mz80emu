797fadce378f711b8565d07112aab7149dca15aa
BoleslawPrus <boleslawPrus@poczta.pl>
2025-03-31 15:28:39 +0200

    Added folder config, unit tests for main file do not work correctly yet.

---
+++ src/unitTests/mainFunctions.h
@@ -0,0 +1,12 @@
+#ifndef TEST_MAINFUNCTIONS
+#define TEST_MAINFUNCTIONS
+
+#include <stdint.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+extern vector<string> splitString(string str, string delimeter);
+
+#endif
