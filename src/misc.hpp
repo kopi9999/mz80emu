37dbea50421309eac0fd5ae13d4bbcd603544feb
BoleslawPrus <BoleslawPrus123>
2025-04-02 10:46:14 +0200

    Fixed misc.hpp.

--- src/misc.hpp
+++ src/misc.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 
-extern bool compareStringVectors(std::vector<std::string>, std::vector<std::string>)
+extern bool compareStringVectors(std::vector<std::string>, std::vector<std::string>);
 extern std::vector<std::string> splitString(std::string, std::string);
 extern bool stringToBool(std::string);
 extern void trim(std::string*);
