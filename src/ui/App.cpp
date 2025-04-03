909bdbf8fde161271662421a615754cf779cada1
HeinrichKamil <hansolo12344321@gmail.com>
2025-04-03 19:51:27 +0200

    corrected mistakes in code

--- src/ui/App.cpp
+++ src/ui/App.cpp
@@ -1,8 +1,9 @@
 #include "App.h"
 #include "MainFrame.h"
 #include <wx/wx.h>
+#define WinMain main
 
-wxIMPLEMENT_APP(App)
+wxIMPLEMENT_APP(App);
 
 bool App::OnInit(){
   MainFrame* mainFrame = new MainFrame("C++ GUI");
---
+++ src/ui/MainFrame.h
@@ -0,0 +1,10 @@
+#ifndef FRAME
+#define FRAME
+#include <wx/wx.h>
+
+class MainFrame : public wxFrame
+{
+    public: MainFrame(const wxString& title);
+};
+
+#endif
