7958e476e9b043a076d5a9b88c5212d21adf7b3c
Dawid <Daw151@users.noreply.github.com>
2025-04-17 11:24:29 +0200

    Added splitByWhitespace function with unit tests, config file accepts various whitespace chars, not only single spaces.

--- src/misc.hpp
+++ src/misc.hpp
@@ -6,6 +6,7 @@
 
 extern bool compareStringVectors(std::vector<std::string>, std::vector<std::string>);
 extern std::vector<std::string> splitString(std::string, std::string);
+extern std::vector<std::string> splitByWhitespace(std::string);
 extern std::string toLower(std::string);
 extern bool stringToBool(std::string);
 extern void trim(std::string*);
