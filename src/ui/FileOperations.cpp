bd86abce5df37e9b076d9475c5c2838afa315f9b
TheDragonEye1 <mlem200@wp.pl>
2025-05-01 18:15:26 +0200

    Add path finder and replace the static one in the ui file :3

--- src/ui/FileOperations.cpp
+++ src/ui/FileOperations.cpp
@@ -6,9 +6,29 @@
 
 std::string ReturnDirectory() 
 {
+    char buffer[MAX_PATH];
+
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+
+    std::string path(buffer);
+
+    std::cout<<buffer<<std::endl;
+
+    size_t lastSlash = path.find_last_of("\\/");
+
+    std::cout<<lastSlash<<std::endl;
+
+    path.resize(lastSlash+1);
+    
+    std::cout<<path<<std::endl;
+
+    path += "*.*";
+
+    std::cout<<path<<std::endl;
+
     WIN32_FIND_DATA file;
     HANDLE file_handle = NULL;
-    std::string path = "E:\\projekt\\mz80emu\\build\\Debug\\*.*";
+    //std::string path = "E:\\projekt\\mz80emu\\build\\Debug\\*.*";
     std::wstring path2(path.begin(), path.end());
     file_handle = FindFirstFile(path2.c_str() , &file);
     std::string output;
