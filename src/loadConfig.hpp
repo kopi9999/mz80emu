39dbb4a653b5bdd9c45a79f04562b3e8c714387f
unknown <Adam Mickiewicz>
2025-05-07 10:30:18 +0200

    deleted some global variables

--- src/loadConfig.hpp
+++ src/loadConfig.hpp
@@ -1,6 +1,6 @@
 #ifndef FILE_OPERATIONS
 #define FILE_OPERATIONS
 
-extern void loadConfig(struct InstanceInfo*, struct InterfacesInfo*, struct ClockInfo*);
+extern void loadConfig(struct Modules* modules, struct InstanceInfo*, struct InterfacesInfo*, struct ClockInfo*);
 
 #endif
