fcce8f8a2de0675fc4ae341e4cb4a248eea8d420
Dawid <Daw151@users.noreply.github.com>
2025-06-14 14:57:04 +0200

    rawDataToFile() function added to config.dll.

--- src/config.hpp
+++ src/config.hpp
@@ -3,6 +3,7 @@
 
 #include "main.hpp"
 
-__declspec(dllexport) enum CrashCode loadConfig(struct Modules* modules, struct InstanceInfo*, struct InterfacesInfo*, struct ClockInfo*);
+__declspec(dllexport) enum CrashCode loadConfig(struct Modules*, struct InstanceInfo*, struct InterfacesInfo*, struct ClockInfo*);
+__declspec(dllexport) enum CrashCode rawDataToFile(std::vector<std::string>, std::vector<std::string>, struct RawInterfacesInfo, struct RawClockInfo);
 
 #endif
