f609cf21268cccbfc710a38ef5ee1ed774a4e46c
TheDragonEyeMuiltiAccount1 <TheDragonEyeMultiAccount@wp.pl>
2025-10-06 09:01:55 +0200

    added files MemoryModule.cpp

---
+++ src/ui/memory_module/MemoryModule.hpp
@@ -0,0 +1,34 @@
+#ifndef FRAME
+#define FRAME
+#include <wx/wx.h>
+#include <wx/listbox.h>
+#include <wx/checkbox.h>
+#include <map>
+
+class MainFrame : public wxFrame
+{
+public:
+    MainFrame(const wxString& title);
+
+private:
+    wxListBox* selectedModulesList;
+    wxListBox* instanceList;
+    wxListBox* interfacesList;
+    wxTextCtrl* clockPeriodInput;
+    wxTextCtrl* clockDepthInput;
+    wxListBox* strobeUpInstanceList;
+    wxListBox* strobeDownInstanceList;
+
+    std::vector<std::string> selectedModules;
+    std::vector<std::string> instanceNames;
+    std::map<int, std::vector<std::string>> instanceInterfaces;
+
+    void OnAddInstance(wxCommandEvent& event);
+    void OnCreateInterfaceArray(wxCommandEvent& event);
+
+    void RefreshModules();
+    void RefreshInstances();
+    void RefreshStrobeLists();
+};
+
+#endif
