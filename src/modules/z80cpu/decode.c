ada79539d5195658c8ee8fc080586ba6f33a9bf3
kopi9999 <przem.gda@gmail.com>
2025-11-10 21:20:31 +0100

    implemented decoding of instructions starting with 00

--- src/modules/z80cpu/decode.c
+++ src/modules/z80cpu/decode.c
@@ -15,11 +15,21 @@ enum CpuState decodeInstruction(struct Instance* __restrict  i)
         }
         else { //00xxxxxx
             switch (i->instruction & 0b00000111) {
-                case 0: 
-                    break;
-                case 1:
-                    break;
-                case 2: 
+                case 0: //00xxx000
+                    switch (i->instruction & 0b00111000){
+                        case 0: return NOP;
+                        case 1: return EX_AF_AF;
+                        case 2: return DJNZ_E;
+                        case 3: return JR_E;
+                        case 4: return JR_NZ_E
+                        case 5: return JR_Z_E;
+                        case 6: return JR_NC_E;
+                        case 7: return JR_C_E;
+                    }
+                case 1: //00xxx001
+                    if (i->instruction & 0b00001000) { return ADD_HL_SS; }
+                    else { return LD_DD_NN; }
+                case 2: //00xxx010
                     if (i->instruction & 0b00001000) {
                         switch (i->instruction & 0b00110000) {
                             case 0: return LD_A_$BC$;
@@ -36,20 +46,29 @@ enum CpuState decodeInstruction(struct Instance* __restrict  i)
                             case 3: return LD_$NN$_A;
                         }
                     }
-                case 3:
+                case 3: //00xxx011
                     if (i->instruction & 0b00001000) {return DEC_SS;}
                     else {return INC_SS;}
-                case 4: 
+                case 4: //00xxx100
                     if ((i->instruction & 0b00111000) == UNDEFINED) {return INC_$HL$;}
                     else {return INC_R;}
-                case 5: 
+                case 5: //00xxx101
                     if ((i->instruction & 0b00111000) == UNDEFINED) {return DEC_$HL$;}
                     else {return DEC_Rp;}
-                case 6: 
+                case 6: //00xxx110
                     if ((i->instruction & 0b00111000) == UNDEFINED) {return LD_$HL$_N;}
                     else {return LD_R_N;}
-                case 7: 
-                    break;
+                case 7: //00xxx111
+                    switch (i->instruction & 0b00111000) {
+                        case 0: return RLCA;
+                        case 1: return RRCA;
+                        case 2: return RLA;
+                        case 3: return RRA;
+                        case 4: return DAA;
+                        case 5: return CPL;
+                        case 6: return SCF;
+                        case 7: return CCF;
+                    }
             }
         }
     }
