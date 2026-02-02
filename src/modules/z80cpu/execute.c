fed77d29e55c2f40dbdd90fc391800944a7909c0
Adam Mickiewicz <adam.mickiewicz@poczta.pl>
2026-02-02 09:48:49 +0100

    improved execute structure

--- src/modules/z80cpu/execute.c
+++ src/modules/z80cpu/execute.c
@@ -1,7 +1,15 @@
 #include "execute.h"
 #include "execute_control.h"
 
-enum Error execute(struct Instance *__restrict i, void **__restrict inf) {
+enum Error execute_up(struct Instance *__restrict i, void **__restrict inf) {
+  switch (i->state) {
+  case HALT: return halt(i);
+  case BAD:  return halt(i);
+  default:   return SUCCESS;
+  }
+}
+
+enum Error execute_down(struct Instance *__restrict i, void **__restrict inf) {
   switch (i->state) {
   case HALT: return halt(i);
   case BAD:  return halt(i);
