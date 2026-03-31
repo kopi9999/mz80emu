4cbfd14a8ade810186006ad66161ed5840d40d3d
Adam Mickiewicz <adam.mickiewicz@poczta.pl>
2026-03-31 12:55:55 +0200

    implemented all instructions from 16 bit load group

--- src/modules/z80cpu/execute_load16.c
+++ src/modules/z80cpu/execute_load16.c
@@ -2,6 +2,7 @@
 #include "execute_control.h"
 
 enum Error ld_dd_nn(struct Instance *__restrict i, void **__restrict inf) {
+  if (i->currentOverride && i->instruction != 0x21){ return BAD_ARGUMENT;}
   if (i->MState == 1) {
     i->MState = 2;
     i->TCycle = 1;
@@ -12,12 +13,20 @@ enum Error ld_dd_nn(struct Instance *__restrict i, void **__restrict inf) {
   }
   if (i->MState == 2) {
     if (i->stateIterator == 0) {
-      switch ((i->instruction & 0b00110000) >> 4) {
-        default: return BAD_ARGUMENT;
-        case BC: i->B = i->tmp; break;
-        case DE: i->D = i->tmp; break;
-        case HL: i->H = i->tmp; break;
-        case SP: i->SP = i->tmp; i->SP = i->SP << 8; break;
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->IX = i->tmp;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->IY = i->tmp;
+      }
+      else {
+	switch ((i->instruction & 0b00110000) >> 4) {
+          default: return BAD_ARGUMENT;
+          case BC: i->C = i->tmp; break;
+          case DE: i->E = i->tmp; break;
+          case HL: i->L = i->tmp; break;
+          case SP: i->SP = i->tmp; break;
+        }
       }
       i->TCycle = 1;
       i->PC++;
@@ -26,12 +35,20 @@ enum Error ld_dd_nn(struct Instance *__restrict i, void **__restrict inf) {
       return SUCCESS;
     }
     if (i->stateIterator == 1) {
-      switch ((i->instruction & 0b00110000) >> 4) {
-        default: return BAD_ARGUMENT;
-        case BC: i->C = i->tmp; break;
-        case DE: i->E = i->tmp; break;
-        case HL: i->L = i->tmp; break;
-        case SP: i->SP |= i->tmp; break;
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->IX = i->tmp << 8;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->IY = i->tmp << 8;
+      }
+      else {
+	switch ((i->instruction & 0b00110000) >> 4) {
+          default: return BAD_ARGUMENT;
+          case BC: i->B = i->tmp; break;
+          case DE: i->D = i->tmp; break;
+          case HL: i->H = i->tmp; break;
+          case SP: i->SP |= i->tmp << 8; break;
+        }
       }
       return nop(i, inf);
     }
@@ -66,14 +83,31 @@ enum Error ld_hl_$nn$(struct Instance *__restrict i, void **__restrict inf) {
       return SUCCESS;
     }
     if (i->stateIterator == 2) {
-      i->L = i->tmp;
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->IX = i->tmp;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->IY = i->tmp;
+      }
+      else {
+	i->L = i->tmp;
+      }      
       i->TCycle = 1;
       *(uint16_t*) inf[0] += 1; //addr
+      i->PC++;
       i->stateIterator = 3;
       return SUCCESS;
     }
     if (i->stateIterator == 3) {
-      i->H = i->tmp;
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->IX |= i->tmp << 8;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->IY |= i->tmp << 8;
+      }
+      else {
+	i->H = i->tmp;
+      }
       return nop(i, inf);
     }
   }
@@ -159,7 +193,15 @@ enum Error ld_$nn$_hl(struct Instance *__restrict i, void **__restrict inf) {
       i->TCycle = 1;
       i->MState = 3;
       *(uint16_t*) inf[0] = i->tmpAddr; //addr
-      i->tmp = i->L;
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->tmp = i->IX & 0x00FF;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->tmp = i->IY & 0x00FF;
+      }
+      else {
+	i->tmp = i->L;
+      }
       i->stateIterator = 2;
       return SUCCESS;
     }
@@ -168,7 +210,15 @@ enum Error ld_$nn$_hl(struct Instance *__restrict i, void **__restrict inf) {
     if (i->stateIterator == 2) {
       i->TCycle = 1;
       *(uint16_t*) inf[0] += 1; //addr
-      i->tmp = i->H;
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->tmp = i->IX >> 8;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->tmp = i->IY >> 8;
+      }
+      else {
+	i->tmp = i->H;
+      }
       i->stateIterator = 3;
       return SUCCESS;
     }
@@ -240,31 +290,50 @@ enum Error ld_$nn$_dd(struct Instance *__restrict i, void **__restrict inf) {
 enum Error ld_sp_hl(struct Instance *__restrict i, void **__restrict inf) {
   if (i->stateIterator == 0) {
     i->stateIterator = 1;
-    i->SP = i->H;
-    i->SP = i->SP << 8;
+    if (i->currentOverride == IX_OVERRIDE) {
+      i->SP = i->IX;
+    }
+    else if (i->currentOverride == IY_OVERRIDE) {
+      i->SP = i->IY;
+    }
+    else {
+      i->SP = i->H;
+      i->SP = i->SP << 8;
+    }
     i->skipTick = 1;
     return SUCCESS;
   }
   if (i->stateIterator == 1) {
-    i->SP |= i->L;
+    if (!i->currentOverride){
+      i->SP |= i->L;
+    }      
     return nop(i, inf);
   }
   return BAD_ARGUMENT;
 }
 
 enum Error push_qq(struct Instance *__restrict i, void **__restrict inf) {
+  if (i->currentOverride && i->instruction != 0xE5){ return BAD_ARGUMENT;}
   if (i->MState == 1) {
     i->MState = 3;
     i->TCycle = 1;
     i->SP--;
     *(uint8_t*) inf[2] = 0; //m1
     *(uint16_t*) inf[0] = i->SP; //addr
-    switch ((i->instruction & 0b00110000) >> 4) {
-      default: return BAD_ARGUMENT;
-      case BC: i->tmp = i->B; break;
-      case DE: i->tmp = i->D; break;
-      case HL: i->tmp = i->H; break;
-      case SP: i->tmp = i->A; break; //AF
+    if (i->currentOverride == IX_OVERRIDE) {
+      i->tmp = i->IX >> 8;
+    }
+    else if (i->currentOverride == IY_OVERRIDE) {
+      i->tmp = i->IY >> 8;
+    }
+    else {
+      switch ((i->instruction & 0b00110000) >> 4) {
+        default: return BAD_ARGUMENT;
+        case BC: i->tmp = i->B; break;
+        case DE: i->tmp = i->D; break;
+        case HL: i->tmp = i->H; break;
+        case SP: i->tmp = i->A; break; //AF
+      }
     }
     i->stateIterator = 0;
     return SUCCESS;
@@ -274,12 +343,20 @@ enum Error push_qq(struct Instance *__restrict i, void **__restrict inf) {
       i->TCycle = 1;
       i->SP--;
       *(uint16_t*) inf[0] -= 1; //addr
-      switch ((i->instruction & 0b00110000) >> 4) {
-        default: return BAD_ARGUMENT;
-        case BC: i->tmp = i->C; break;
-        case DE: i->tmp = i->E; break;
-        case HL: i->tmp = i->L; break;
-        case SP: i->tmp = i->F; break; //AF
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->tmp = i->IX & 0x00FF;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->tmp = i->IY & 0x00FF;
+      }
+      else {
+	switch ((i->instruction & 0b00110000) >> 4) {
+          default: return BAD_ARGUMENT;
+          case BC: i->tmp = i->C; break;
+          case DE: i->tmp = i->E; break;
+          case HL: i->tmp = i->L; break;
+          case SP: i->tmp = i->F; break; //AF
+        }
       }
       i->stateIterator = 1;
       return SUCCESS;
@@ -305,24 +382,40 @@ enum Error pop_qq(struct Instance *__restrict i, void **__restrict inf) {
       i->TCycle = 1;
       i->SP++;
       *(uint16_t*) inf[0] += 1; //addr
-      switch ((i->instruction & 0b00110000) >> 4) {
-        default: return BAD_ARGUMENT;
-        case BC: i->tmp = i->C; break;
-        case DE: i->tmp = i->E; break;
-        case HL: i->tmp = i->L; break;
-        case SP: i->tmp = i->F; break; //AF
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->tmp = i->IX & 0x00FF;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->tmp = i->IY & 0x00FF;
+      }
+      else {
+	switch ((i->instruction & 0b00110000) >> 4) {
+          default: return BAD_ARGUMENT;
+          case BC: i->tmp = i->C; break;
+          case DE: i->tmp = i->E; break;
+          case HL: i->tmp = i->L; break;
+          case SP: i->tmp = i->F; break; //AF
+        }
       }
       i->stateIterator = 1;
       return SUCCESS;
     }
     if (i->stateIterator == 1) {
       i->SP++;
-      switch ((i->instruction & 0b00110000) >> 4) {
-        default: return BAD_ARGUMENT;
-        case BC: i->tmp = i->B; break;
-        case DE: i->tmp = i->D; break;
-        case HL: i->tmp = i->H; break;
-        case SP: i->tmp = i->A; break; //AF
+      if (i->currentOverride == IX_OVERRIDE) {
+	i->tmp = i->IX >> 8;
+      }
+      else if (i->currentOverride == IY_OVERRIDE) {
+	i->tmp = i->IY >> 8;
+      }
+      else {
+	switch ((i->instruction & 0b00110000) >> 4) {
+          default: return BAD_ARGUMENT;
+          case BC: i->tmp = i->B; break;
+          case DE: i->tmp = i->D; break;
+          case HL: i->tmp = i->H; break;
+          case SP: i->tmp = i->A; break; //AF
+        }
       }
       return nop(i, inf);
     }
