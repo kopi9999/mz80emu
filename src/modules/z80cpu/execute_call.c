dd4f4569a96e063fbcbe9e5ff8884035cb2993b3
Adam Mickiewicz <adam.mickiewicz@poczta.pl>
2026-04-01 19:00:06 +0200

    added file for call group operations

---
+++ src/modules/z80cpu/execute_call.c
@@ -0,0 +1,6 @@
+#include "execute_call.h"
+#include "execute_control.h"
+
+enum Error call_nn(struct Instance* __restrict i, void** __restrict inf) {
+  return SUCCESS;
+}
---
+++ src/modules/z80cpu/execute_call.h
@@ -0,0 +1,8 @@
+#ifndef EXEC_CALL
+#define EXEC_CALL
+
+#include "z80cpuMod.h"
+
+enum Error call_nn(struct Instance* __restrict i, void** __restrict inf);
+
+#endif
